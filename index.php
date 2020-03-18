5643c81c28db54ca7947073a7e5f3cd8fbdc320a
Кирилл Курдюков <kirillkurdukov243@gmail.com>
2020-03-18 19:26:33 +0300

    second task

--- index.php
+++ index.php
@@ -116,27 +116,26 @@ $name_list =[ [
             <div class="lots__header">
                 <h2>Открытые лоты</h2>
             </div>
-            <ul class="lots__list" <?php foreach ($name_list as $key => $value): ?>
-                <!--заполните этот список из массива с товарами-->
-                <li class="lots__item lot">
+            <ul class="lots__list"><?php foreach ($name_list as $k => $val) : ?>
+                    <!--заполните этот список из массива с товарами-->
+                    <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src=<?php print ($value['image']); ?> width="350" height="260" alt=<?php print ($value['name']); ?>
-                        </div>
-                        <div class="lot__info">
-                            <span class="lot__category"><?=$category_list[$value['category']] ?> </span>
-                            <h3 class="lot__title"><a class="text-link" href="pages/lot.html"> <?php print($value['name']); ?> </a></h3>
-                            <div class="lot__state">
-                                <div class="lot__rate">
-                                    <span class="lot__amount">Стартовая цена</span>
-                                    <span class="lot__cost"> <?php print ($value['price']); ?><b class="rub">р</b></span>
-                                </div>
-                                <div class="lot__timer timer">
-                                    12:23
-                                </div>
+                        <img src="img/lot-<?=($k + 1)?>.jpg" width="350" height="260" alt="<?=$val['name']?>">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?=$category_list[$val['category']]?></span>
+                        <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$val['name']?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?=$val['price']?><b class="rub">р</b></span>
+                            </div>
+                            <div class="lot__timer timer">
+                                12:23
                             </div>
                         </div>
-                </li>
-            <?php endforeach; ?>
+                    </div>
+                    </li><?php endforeach; ?>
             </ul>
         </section>
     </main>
